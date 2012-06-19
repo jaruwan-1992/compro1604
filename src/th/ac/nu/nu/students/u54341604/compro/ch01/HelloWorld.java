b68a8309f8cd722cacd99265016395b689acb1a6
jaruwan-1992 <paple_22@hotmail.com>
2012-06-19 16:30:58 +0700

    new Hello World

---
+++ src/th/ac/nu/nu/students/u54341604/compro/ch01/HelloWorld.java
@@ -0,0 +1,9 @@
+
+package th.ac.nu.nu.students.u54341604.compro.ch01;
+
+public class HelloWorld {
+    public static void main(String[]args){
+        System.out.println("Hello,World");        
+    }
+    
+}
