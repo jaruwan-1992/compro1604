fc298303af55feb52256f8aa7bf321345eb66ba2
jaruwan-1992 <paple_22@hotmail.com>
2012-06-19 16:34:01 +0700

    เพิ่ม "Programming is fun"

--- src/th/ac/nu/nu/students/u54341604/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/nu/students/u54341604/compro/ch01/HelloWorld.java
@@ -3,7 +3,8 @@ package th.ac.nu.nu.students.u54341604.compro.ch01;
 
 public class HelloWorld {
     public static void main(String[]args){
-        System.out.println("Hello,World");        
+        System.out.println("Hello,World");
+        System.out.println("Programming is fun");
     }
     
 }
