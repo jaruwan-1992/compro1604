768d19b1144831171ac21c9ef6b446edacf27b98
jaruwan-1992 <paple_22@hotmail.com>
2012-07-10 15:44:52 +0700

    เพิ่ม Chapter2

---
+++ src/th/ac/nu/nu/students/u54341604/compro/ch02/Videning1.java
@@ -0,0 +1,11 @@
+package th.ac.nu.nu.students.u54341604.compro.ch02;
+
+public class Videning1 {
+   public static void main (String [] args){
+   int i = 1234567890;
+   float f = i;
+System.out.println(i - (int) f);
+}
+
+}   
+
