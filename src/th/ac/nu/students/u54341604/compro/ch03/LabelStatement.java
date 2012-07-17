63b8903974ff7a9fe3702cd1866a5d3eda9e6333
jaruwan-1992 <paple_22@hotmail.com>
2012-07-17 14:34:20 +0700

    Chapter 3

---
+++ src/th/ac/nu/students/u54341604/compro/ch03/LabelStatement.java
@@ -0,0 +1,11 @@
+package th.ac.nu.students.u54341604.compro.ch03;
+
+public class LabelStatement {
+    public static void main(String[] args) {
+    first : {
+        System.out.print("Hello, ");
+        here : System.out.println("World!");
+        }
+     }
+ }   
+
